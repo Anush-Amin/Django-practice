0e5a800a64decbaaa649030d84a2705a580cb751
Anush Amin <anush.mlore@gmail.com>
2023-09-01 23:55:08 +0530

    Commit after video 43 of Section 4

--- README.md
+++ README.md
@@ -28,3 +28,15 @@ def index(request):
 12) Create a urls.py file inside challenges and add code there
 13) Now add code to urls.py file in monthly_challenges folder
 14) Redirects: 
+15) To make use of html files, create a templates folder inside the app folder (here app folder we have created is challenges) and inside this folder create the html file
+16) In order to make this file to be used, go to settings.py file in the monthly_challenges folder. We can make the template be detected by adding "DIRS":[BASE_DIR / "challenges" / "templates"] inside TEMPLATES. This step is useful to detect a template which is used for all apps.
+If we have to make the templates of all apps created to be detected then in settings.py file add the app name inside INSTALLED_APPS list. Make sure that "APP_DIRS": True in TEMPLATES
+17) For creating the html file inside app folder it is kept in the folder templates>appname>html file. Here appname folder (should have name like if challenges is the app then templates>challenges>html file) is created to avoid confusion in detecting the html files with same name while rendering the template. Hence its the best practice to keep templates in this format
+18) DTL: enhanced HTML files to create dynamic pages
+Standard HTML syntax + Special DTL Syntax = Dynamic HTML page
+* {{ variable }} : we can call the variable from python to html using curly braces
+* filter: there are many built in filter which can be used
+For example: {{variable|title}} if variable is "my FIRST post", the output will be "My First Post"
+* tags: for tags is used for looping, url tag is used for constructing url
+19) Template inheritance: Instead of writing the entire skeleton for all html files, we can create a html file which contains basic skeleton and this can be used by all other html files. Create this base.html file in templates folder. Use block tag where other files need to have dynamic changes. Use {% extends 'base.html' %} in the html files which uses base.html
+Note: the path given here for base.html is absolute. To make django recognize this path, in monthly_challenges -> settings.py add BASE_DIR / "templates" in TEMPLATES list
--- challenges/views.py
+++ challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     "january": "Eat no meat for entire month",
@@ -14,7 +15,7 @@ monthly_challenges = {
     "september": "Learn Django for atleast 20 min everyday!",
     "october": "Eat no meat for entire month",
     "november": "Walk for atleast 20 min everyday!",
-    "december": "Learn Django for atleast 20 min everyday!"
+    "december": None
 }
 
 # Create your views here.
@@ -29,16 +30,21 @@ monthly_challenges = {
 #    return HttpResponse("Learn Django for atleast 20 min everyday!")
 
 def index(request):
-    list_items = ""
+    # list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
+    ## note: ctrl+/ to comment multiple lines
+    ## instead of using for loop here we can use the for tag in index.html page
+    return render(request, "challenges/index.html", {
+        "months": months # this "months" will be used in index.html for tags
+    })
 
 def monthly_challenge_by_number(request, month):
     """Here if number is entered in the URL it will
@@ -59,8 +65,13 @@ def monthly_challenge_by_number(request, month):
 def monthly_challenge(request, month):
     try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
+        #response_data = render_to_string("challenges/challenge.html")
+        #return HttpResponse(response_data)
+        ## instead of using above 2 lines we can use
+        return render(request, "challenges/challenge.html", {
+             "text": challenge_text,
+             "month_name": month
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
 
